94d2e5d14609923605f6f4f611fb885aac7f55e4
Bittens <Bittens.M@bgr.local>
2023-02-10 13:58:02 +0100

    ins

---
+++ src/AltInplaceOperationInterface.jl
@@ -0,0 +1,11 @@
+module AltInplaceOperationInterface
+
+add!(a::A, b::B) where {A,B} = error("add!(a::$A, b::$B) is not implemented")
+minus!(a::A, b::B) where {A,B} = error("minus!(a::$A, b::$B) is not implemented")
+pow!(a::A, b::B) where {A,B} = error("pow!(a::$A, b::$B) is not implemented")
+max!(a::A, b::B) where {A,B} = error("max!(a::$A, b::$B) is not implemented")
+min!(a::A, b::B) where {A,B} = error("min!(a::$A, b::$B) is not implemented")
+
+export: add!, minus!, pow!, max!, min!
+
+end
